ed546b7409508c5680e04d39d1f63ed03b8ca4f7
CosmeNicolas <nicomas7590@gmail.com>
2024-03-11 17:41:54 -0300

    configuramos un archivo estático en la ruta principal del proyecto

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //importamos express 
-import express  from 'express'
+import express, { urlencoded }  from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 //configuar puerto 
@@ -14,10 +16,18 @@ import morgan from 'morgan';
 //config middleware
 app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json()); //permite interpretar en formato json
+app.use(express.urlencoded({extended:true}))//interpreta los datos del body del request
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname,'/public'))) 
 
 
 //config  rutas , http://localhost:3000/productos
-app.get('/', (req, res)=>{
+app.get('/nuevo', (req, res)=>{
     console.log('procesando respuesta')
     res.send('respuesta del back de tareas ')
-})
+});
+
+ 
+
