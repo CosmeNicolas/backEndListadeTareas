29739c063d6ce042862c950468f10201e2601a4f
CosmeNicolas <nicomas7590@gmail.com>
2024-03-25 17:45:57 -0300

    cambie el numero del puerto de 4001 > 4004 y saque los comentarios

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//importamos express 
 import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
@@ -9,31 +8,21 @@ import listarTareas from './src/routes/tareas.routes.js';
 import './src/database/database.js'
 
 
-//configuar puerto 
+
  const app = express();
  app.set('port', process.env.PORT || 4000);
  app.listen(app.get('port'),()=>{
     console.log('estoy en el puerto' + app.get('port'))
  })
-//config middleware
+
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.json()); //permite interpretar en formato json
-app.use(express.urlencoded({extended:true}))//interpreta los datos del body del request
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname,'/public'))) 
 
 
-//config  rutas , http://localhost:3000/productos
+
 app.use('/api', listarTareas) 
-
-
-
-/* app.get('/nuevo', (req, res)=>{
-    console.log('procesando respuesta')
-    res.send('respuesta del back de tareas ')
-});
- */
- 
-
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { borrarTarea, crearTarea, editarTarea, listarTareas, obtenerTarea } from "../controllers/tareas.controllers.js";
 
 const router = Router()
-//como creo las rutas 
 
 router.route('/tareas').get(listarTareas).post(crearTarea)
 router.route('/tareas/:id').get(obtenerTarea).put(editarTarea).delete(borrarTarea)
